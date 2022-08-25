e44f948bd8cc16766f2a6bf236771e6398570a3d
sondonghwan <isonpro@gmail.com>
2022-08-25 23:24:38 +0900

    Initial commit

---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  late String questionText;
+  late bool questionAnswer;
+
+  Question(this.questionText, this.questionAnswer);
+// {
+// questionText = q;
+// questionAnswer = a;
+// }
+
+}
